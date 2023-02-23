ae64538a9490a69bc652eeed423549185b97909d
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-23 13:39:39 +0700

    Update depznem.lua

--- depznem.lua
+++ depznem.lua
@@ -78,5 +78,6 @@ ListPembeli = {
     {iddc = "547032936453767169", nick = "dranest27"},
     {iddc = "614877382352044073", nick = "jujip1"},
     {iddc = "754633913439354901", nick = "move6ahead"},
-    {iddc = "757878410352525353", nick = "veraxcuy"}
+    {iddc = "757878410352525353", nick = "veraxcuy"},
+    {iddc = "515957623078649864", nick = "PaintGoNJRI"}
 }
